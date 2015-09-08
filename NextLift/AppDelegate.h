0f9420dc6b97e892291ac8946fadaf09d51ef011
daveloper87 <dai.williams@bbc.co.uk>
2015-09-08 23:14:13 +0100

    Initial Commit

---
+++ NextLift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NextLift
+//
+//  Created by Dai Williams on 08/09/2015.
+//  Copyright (c) 2015 daio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NextLift/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NextLift
+//
+//  Created by Dai Williams on 08/09/2015.
+//  Copyright (c) 2015 daio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
