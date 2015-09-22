075dbbed97cbe5f9833c793ed4693dc619422513
daveloper87 <dai.williams@bbc.co.uk>
2015-09-22 19:06:54 +0100

    Started moving everything into collection views

---
+++ NextLift/MuscleGroupsViewController.h
@@ -0,0 +1,14 @@
+//
+//  MuscleGroupsViewController.h
+//  NextLift
+//
+//  Created by Dai Williams on 22/09/2015.
+//  Copyright © 2015 daio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NLFDatabase.h"
+
+@interface MuscleGroupsViewController : UICollectionViewController <NLFDatabaseDelegate>
+
+@end
